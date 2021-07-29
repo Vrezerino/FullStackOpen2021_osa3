f47b5595a556e27b3fcd59e5534b08ff4b4004fa
Vrezerino <parkpatrick1@gmail.com>
2021-07-29 22:17:23 +0300

    done

--- index.js
+++ index.js
@@ -13,66 +13,66 @@ morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status - :response-time ms :body'))
 
 const errorHandler = (e, req, res, next) => {
-  console.error(e.message)
-  if (e.name === 'CastError') {
-    return res.status(400).send(e.message)
-  } else if (e.name === 'ValidationError') {
-    return res.status(400).send('Name must be longer than 3 characters and number longer than 8!')
-  }
-  next(e)
+	console.error(e.message)
+	if (e.name === 'CastError') {
+		return res.status(400).send(e.message)
+	} else if (e.name === 'ValidationError') {
+		return res.status(400).send('Name must be longer than 3 characters and number longer than 8!')
+	}
+	next(e)
 }
 
 app.get('/api/persons', (req, res, next) => {
-  Person.find({}).then(people => res.json(people))
-  .catch(e => next(e))
+	Person.find({}).then(people => res.json(people))
+		.catch(e => next(e))
 })
 
 app.get('/api/persons/:id', (req, res, next) => {
-  Person.findById(req.params.id).then(p => {
-    if (p) {
-      res.json(p)
-    } else {
-      res.status(404).send('404 Not Found: Person with that ID does not exist!')
-    }
-  })
-  .catch(e => next(e))
+	Person.findById(req.params.id).then(p => {
+		if (p) {
+			res.json(p)
+		} else {
+			res.status(404).send('404 Not Found: Person with that ID does not exist!')
+		}
+	})
+		.catch(e => next(e))
 })
 
 app.delete('/api/persons/:id', (req, res, next) => {
-  Person.findByIdAndRemove(req.params.id)
-    .then(result => res.status(204).end())
-    .catch(e => next(e))
+	Person.findByIdAndRemove(req.params.id)
+		.then(() => res.status(204).end())
+		.catch(e => next(e))
 })
 
 app.post('/api/persons/', (req, res, next) => {
-  const body = req.body
-  const person = new Person({
-    name: body.name,
-    number: body.number
-  })
-  person.save().then(p => res.json(p))
-  .catch(e => next(e))
+	const body = req.body
+	const person = new Person({
+		name: body.name,
+		number: body.number
+	})
+	person.save().then(p => res.json(p))
+		.catch(e => next(e))
 })
 
 app.put('/api/persons/:id', (req, res, next) => {
-  const body = req.body
-  const person = {
-    name: body.name,
-    number: body.number
-  }
-  Person.findByIdAndUpdate(req.params.id, person, { new: true, runValidators: true, context: 'query' })
-  .then(updatedPerson => res.json(updatedPerson))
-  .catch(e => next(e))
+	const body = req.body
+	const person = {
+		name: body.name,
+		number: body.number
+	}
+	Person.findByIdAndUpdate(req.params.id, person, { new: true, runValidators: true, context: 'query' })
+		.then(updatedPerson => res.json(updatedPerson))
+		.catch(e => next(e))
 })
 
-app.get('/info', (req, res) => {
-  Person.countDocuments({}, (e, count) => {
-    res.send(`
+app.get('/info', (req, res, next) => {
+	Person.countDocuments({}, (e, count) => {
+		res.send(`
     There are <b>${count}</b> people in the phonebook.
     <p>${new Date()}</p>
     `)
-  })
-  .catch(e => next(e))
+	})
+		.catch(e => next(e))
 })
 
 const notFound = (req, res) => res.status(404).send('Error 404: Not found!')
@@ -80,7 +80,8 @@ const notFound = (req, res) => res.status(404).send('Error 404: Not found!')
 app.use(notFound)
 app.use(errorHandler)
 
+// eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Running on ${PORT}`)
+	console.log(`Running on ${PORT}`)
 })
--- mongo.js
+++ mongo.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 const mongoose = require('mongoose')
 
 const password = process.argv[2]
@@ -5,29 +7,29 @@ const url = `mongodb+srv://superadmin:${password}@cluster0.o7ojv.mongodb.net/puh
 mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true})
 
 const schema = new mongoose.Schema({
-  name: String,
-  number: String
+	name: String,
+	number: String
 })
 
 const Person = mongoose.model('Person', schema)
 const person = new Person({
-  name: process.argv[3],
-  number: process.argv[4]
+	name: process.argv[3],
+	number: process.argv[4]
 })
 
 
 if (process.argv.length > 3) {
-  person.save().then(r => {
-    console.log(`Added ${process.argv[3]} (${process.argv[4]}) to phonebook!`)
-    mongoose.connection.close()
-  })
-  .catch(e => console.log(e))
+	person.save().then(r => {
+		console.log(`Added ${process.argv[3]} (${process.argv[4]}) to phonebook!`)
+		mongoose.connection.close()
+	})
+		.catch(e => console.log(e))
 } else {
-  Person.find({}).then(r => {
-    if (r.length === 0) console.log(`Sure is empty around here`)
-    r.forEach(p => {
-      console.log(p)
-    })
-    mongoose.connection.close()
-  })
+	Person.find({}).then(r => {
+		if (r.length === 0) console.log('Sure is empty around here')
+		r.forEach(p => {
+			console.log(p)
+		})
+		mongoose.connection.close()
+	})
 }
